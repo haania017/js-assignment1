fc98de42deb2c6da3038decb9cce7d11870da01c
Haania <pc1336.haania@gmail.com>
2026-05-03 12:07:39 +0500

    Add files via upload

---
+++ Simple Calculator/calc.js
@@ -0,0 +1,8 @@
+var num1 = parseInt(prompt("Enter your first number:"));
+var num2 = parseInt(prompt("Enter your second number:"));
+
+alert(`Addition: ${num1 + num2}`);
+alert(`Subtraction: ${num1 - num2}`);
+alert(`Multiplication: ${num1 * num2}`);
+alert(`Division: ${num1 / num2}`);
+alert(`Remainder: ${num1 % num2}`);
